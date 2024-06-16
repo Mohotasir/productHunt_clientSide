fe73c9dd68df0cd726b553e8d6b595894e1a92d8
mohotasir <mohotasir1036@gmail.com>
2024-06-16 14:08:19 +0600

    add cilp in slider

--- src/Pages/HOME/Slider/Slider.jsx
+++ src/Pages/HOME/Slider/Slider.jsx
@@ -6,13 +6,13 @@ import 'swiper/css/navigation';
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/effect-fade';
 import './banner.css';
-import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import { Autoplay } from 'swiper/modules';
 
 const Slider = () => {
     return (
-        <div className="bg-cover bgClr bg-no-repeat border bg-blue-50">
+        <div className="clip bg-cover bgClr bg-no-repeat  bg-slate-50 ">
             <div className='container py-6 md:py-0 mx-auto  items-center justify-center  '>
-                <div className='m-8 max-w-screen-lg mx-auto mulish'>
+                <div className=' max-w-screen-lg mx-auto mulish'>
                     <Swiper
                         spaceBetween={30}
                         centeredSlides={true}
@@ -21,28 +21,28 @@ const Slider = () => {
                             disableOnInteraction: false,
                         }}
                         pagination={{
-                            clickable: true,
+                          
                         }}
-                        modules={[Autoplay, Pagination]}
-                        className="mySwiper rounded-2xl h-[40vh]"
+                        modules={[Autoplay]}
+                        className="mySwiper rounded-2xl h-[40vh] lg:h-[80vh]"
                     >
-                        <SwiperSlide className='bg-cover  p-12 bg-slate-50 bg-no-repeat'>
+                        <SwiperSlide className='bg-cover   lg:-mt-12   p-12 bg-slate-50 bg-no-repeat'>
                             <div className='w-full'>
-                                <h1 className='text-2xl text-gray-600 lg:text-5xl font-bold '>Welcome to TRENDZ</h1>
+                                <h1 className='text-4xl text-gray-500 lg:text-7xl font-bold '>Hunt Your Product in <span  className='text-blue-300'>TRENDZ</span> </h1>
                                 <p className=' md:text-2xl my-4 max-w-[600px] font-light text-blue-900 '>Choose your desired product! Make your work easier with Technology and AI.</p>
                                 <p className='btn bg-blue-50'>Explore Products</p>
                             </div>
                         </SwiperSlide>
-                        <SwiperSlide className='bg-cover p-12 bg-slate-50 bg-no-repeat'>
+                        <SwiperSlide className='bg-cover lg:-mt-12  p-12 bg-slate-50 bg-no-repeat'>
                             <div className='w-full'>
-                                <h1 className='text-2xl lg:text-5xl font-bold text-pink-500'>Use COUPON & Get Discount</h1>
+                                <h1 className='text-4xl lg:text-7xl font-bold text-gray-500'>Use <span className='text-blue-300'>COUPON</span> & Get Discount</h1>
                                 <p className='md:text-2xl my-4 max-w-[600px] font-light text-blue-900 '>Choose your desired product! Make your work easier with Technology and AI.</p>
                                 <p className='btn bg-blue-50'>Explore Products</p>
                             </div>
                         </SwiperSlide>
-                        <SwiperSlide className='bg-cover p-12 bg-slate-50 bg-no-repeat'>
+                        <SwiperSlide className='bg-cover lg:-mt-12  p-12 bg-slate-50 bg-no-repeat'>
                             <div className='w-full'>
-                                <h1 className='text-2xl text-purple-400 lg:text-5xl font-bold '>Make Life Easy with AI</h1>
+                            <h1 className='text-4xl text-gray-500 lg:text-7xl font-bold '>Make Life Easy with <span className='text-blue-300'>AI</span> </h1>
                                 <p className='md:text-2xl my-4 max-w-[600px] font-light text-blue-900 '>Choose your desired product! Make your work easier with Technology and AI.</p>
                                 <p className='btn bg-blue-50'>Explore Products</p>
                             </div>
